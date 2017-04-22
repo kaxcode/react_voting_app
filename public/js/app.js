be313f525342747285a40fb676e6cf4ba0781e51
Kenia Valladarez <keniavalladarez@gmail.com>
2017-04-21 23:29:59 -0400

    renders multiple products from the seed file

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,8 @@
 class ProductList extends React.Component {
   render() {
-    const product = Seed.products[0];
-    return (
-      <div className='ui unstackable items'>
-        <Product
+    const productComponents = Seed.products.map((product) => (
+      <Product
+        key={'product-' + product.id}
         id={product.id}
         title={product.title}
         description={product.description}
@@ -11,7 +10,11 @@ class ProductList extends React.Component {
         votes={product.votes}
         submitterAvatarUrl={product.submitterAvatarUrl}
         productImageUrl={product.productImageUrl}
-        />
+      />
+    ));
+    return (
+      <div className='ui unstackable items'>
+        {productComponents}
       </div>
     );
   }
