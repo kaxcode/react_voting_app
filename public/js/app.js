ba292c7398989153043c828a1d6c9ebd2cb9b37d
Kenia Valladarez <keniavalladarez@gmail.com>
2017-04-21 22:34:49 -0400

    Passes the first product form the seed file into product list

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,55 @@
-class ProductList extends React.Component {
-  render() {
+const ProductList = React.createClass({
+  render: function () {
+    const product = Data[0];
     return (
-      <div className='ui unstackable items'>
-        <Product />
+      <div className='ui items'>
+        <Product
+          id={product.id}
+          title={product.title}
+          description={product.description}
+          url={product.url}
+          votes={product.votes}
+          submitter_avatar_url={product.submitter_avatar_url}
+          product_image_url={product.product_image_url}
+        />
       </div>
     );
-  }
-}
+  },
+});
 
-class Product extends React.Component {
-  render() {
+
+const Product = React.createClass({
+  render: function () {
     return (
       <div className='item'>
         <div className='image'>
-          <img src='images/products/image-aqua.png' />
+          <img src={this.props.product_image_url} />
         </div>
         <div className='middle aligned content'>
+          <div className='header'>
+            <a>
+              <i className='large caret up icon'></i>
+            </a>
+            {this.props.votes}
+          </div>
           <div className='description'>
-            <a>Fort Knight</a>
-            <p>Authentic renaissance actors, delivered in just two weeks.</p>
+            <a href={this.props.url}>
+              {this.props.title}
+            </a>
           </div>
           <div className='extra'>
             <span>Submitted by:</span>
             <img
               className='ui avatar image'
-              src='images/avatars/daniel.jpg'
+              src={this.props.submitter_avatar_url}
             />
           </div>
         </div>
       </div>
     );
-  }
-}
+  },
+});
+
 
 ReactDOM.render(
   <ProductList />,
