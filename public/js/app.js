c986aa52b6ca9bd9e42bcaa96eb9af914d39acf6
Kenia Valladarez <keniavalladarez@gmail.com>
2017-04-22 22:04:42 -0400

    Adds product function to handle the upvote functions

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,14 @@
+
 class ProductList extends React.Component {
   handleProductUpVote(productId) {
     console.log(productId + ' was upvoted.');
   }
-  
+
   render() {
     const products = Seed.products.sort((a, b) => (
       b.votes - a.votes
     ));
-    const productComponents = Seed.products.map((product) => (
+    const productComponents = products.map((product) => (
       <Product
         key={'product-' + product.id}
         id={product.id}
@@ -17,6 +18,7 @@ class ProductList extends React.Component {
         votes={product.votes}
         submitterAvatarUrl={product.submitterAvatarUrl}
         productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
       />
     ));
     return (
@@ -28,16 +30,28 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+
+  // Inside `Product`
+  handleUpVote() {
+    this.props.onVote(this.props.id);
+  }
+
   render() {
     return (
       <div className='item'>
         <div className='image'>
           <img src={this.props.productImageUrl} />
         </div>
+        {/* Inside `render` for Product` */}
         <div className='middle aligned content'>
           <div className='header'>
-            <a>
-              <i className='large caret up icon'></i>
+            <a onClick={this.handleUpVote}>
+              <i className='large caret up icon' />
             </a>
             {this.props.votes}
           </div>
@@ -46,7 +60,7 @@ class Product extends React.Component {
               {this.props.title}
             </a>
             <p>
-            {this.props.description}
+              {this.props.description}
             </p>
           </div>
           <div className='extra'>
