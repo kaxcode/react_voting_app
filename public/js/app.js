adeff9e159e6e7b2aaa2c3de085867e12801b25f
Kenia Valladarez <keniavalladarez@gmail.com>
2018-01-02 22:49:03 -0500

    Creates new component with ES6 and ReactDOM to render.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,14 @@
+class ProductList extends React.Component {
+	render() {
+		return (
+			<div className="ui unstackale items">
+				Hello, friends! I'm a basic component
+			</div>
+		)
+	}
+}
 
+ReactDOM.render(
+	<ProductList />,
+	document.getElementById('content')
+);
