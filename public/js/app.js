b88b5a27736d14642c2f615b298f99be9390a009
Kenia Valladarez <kenia.valladarez@cloudreach.com>
2018-01-08 11:34:54 -0500

    Implement the state into our custom method.

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,18 @@ class ProductList extends React.Component {
 	}
 
 	handleProdcutUpVote(productId) {
-		console.log(productId + ' was upvoted.');
+		const nextProducts = this.state.products.map((product) => {
+			if (product.id === productId) {
+				return Object.assign({}, product, {
+					votes: product.votes + 1,
+				});
+			} else {
+				return product;
+			}
+		});
+		this.setState({
+			products: nextProducts,
+		});
 	}
 	
 	render() {
